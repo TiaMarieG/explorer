5710b1c9a97bf8b94f28e163aea840e30f45aac8
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-04-06 13:47:56 -0700

    More tests

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -1,4 +1,8 @@
 import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.Set;
+
 import org.junit.Test;
 
 public class ExplorerSearchTest {
@@ -83,5 +87,68 @@ public class ExplorerSearchTest {
             ExplorerSearch.explorerLocation(island);
         });
         assertEquals("No explorer present", exception.getMessage());
+    }
+
+    @Test
+    public void testPossibleMoves_allDirectionsOpen() {
+        int[][] island = {
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,0,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+        };
+        int[] location = {2, 2};
+        List<int[]> moves = ExplorerSearch.possibleMoves(island, location);
+        Set<String> moveSet = toSet(moves);
+
+        assertEquals(4, moves.size());
+        assertTrue(moveSet.contains("1,2")); // up
+        assertTrue(moveSet.contains("3,2")); // down
+        assertTrue(moveSet.contains("2,1")); // left
+        assertTrue(moveSet.contains("2,3")); // right
+    }
+
+    @Test
+    public void testPossibleMoves_allDirectionsBlocked() {
+        int[][] island = {
+            {1,1,1,1,1,1},
+            {1,3,2,3,1,1},
+            {1,2,0,2,1,1},
+            {1,3,2,3,1,1},
+            {1,1,1,1,1,1},
+        };
+        int[] location = {2, 2};
+        List<int[]> moves = ExplorerSearch.possibleMoves(island, location);
+        assertTrue(moves.isEmpty());
+    }
+
+    @Test
+    public void testPossibleMoves_partialEdge() {
+        int[][] island = {
+            {1,1,1,1,1,0}
+        };
+        int[] location = {0, 5};
+        List<int[]> moves = ExplorerSearch.possibleMoves(island, location);
+        Set<String> moveSet = toSet(moves);
+
+        assertEquals(1, moves.size());
+        assertTrue(moveSet.contains("0,4")); // only left
+    }
+
+    @Test
+    public void testPossibleMoves_twoWalls() {
+        int[][] island = {
+            {3,2,3,2,3,2},
+            {1,1,0,1,1,1},
+            {2,3,2,3,2,3}
+        };
+        int[] location = {1, 2};
+        List<int[]> moves = ExplorerSearch.possibleMoves(island, location);
+        Set<String> moveSet = toSet(moves);
+
+        assertEquals(4, moves.size());
+        assertTrue(moveSet.contains("1,1")); // left
+        assertTrue(moveSet.contains("1,3")); // right
     }
 }
