cf9fcf671ab579e043368a7aa9057c1e7a6036ba
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-04-06 13:36:38 -0700

    Began creating test cases

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -17,4 +17,71 @@ public class ExplorerSearchTest {
 
     // Add more tests here!
     // Come up with varied cases
+
+    @Test
+    public void testReachableArea_allReachable() {
+        int[][] island = {
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,0,1},
+            {1,1,1,1,1,1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(30, actual);
+    }
+
+    @Test
+    public void testReachableArea_allUnreachable() {
+        int[][] island = {
+            {3,2,3,2,3,2},
+            {2,3,2,3,2,3},
+            {3,2,3,2,3,2},
+            {3,2,3,2,0,3},
+            {2,3,2,3,2,3},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(30, actual);
+    }
+
+    @Test
+    public void testExplorerArea_offCenterGrid() {
+        int[][] island = {
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,0,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+        };
+        int[] expected = {2, 3};
+        assertArrayEquals(expected, ExplorerSearch.explorerLocation(island));
+    }
+
+    @Test
+    public void testExplorerArea_bottomRightGrid() {
+        int[][] island = {
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,0},
+        };
+        int[] expected = {4, 5};
+        assertArrayEquals(expected, ExplorerSearch.explorerLocation(island));
+    }
+
+    @Test
+    public void testExplorerArea_explorerMissing_throwsException() {
+        int[][] island = {
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+        };
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ExplorerSearch.explorerLocation(island);
+        });
+        assertEquals("No explorer present", exception.getMessage());
+    }
 }
